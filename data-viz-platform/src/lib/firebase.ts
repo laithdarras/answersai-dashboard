687eefbbba250f6c8529c215530d4f51891abbcd
Laith Darras <laith.s.darras@gmail.com>
2025-06-19 16:18:52 -0700

    moved firebase config to .env for security

--- data-viz-platform/src/lib/firebase.ts
+++ data-viz-platform/src/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB_yNLoZ8w7tXcZ4eBQ2aeDmNhMQvDTxfQ',
-  authDomain: 'answersai-c659e.firebaseapp.com',
-  projectId: 'answersai-c659e',
-  storageBucket: 'answersai-c659e.appspot.com',
-  messagingSenderId: '231126996141',
-  appId: '1:231126996141:web:1a38ac1f2eb04dfdd86224',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
-
 export const auth = getAuth(app)
